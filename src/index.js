3d6c1a4e3f6dca09ee063c528a8498cf898405f8
Sumit Kumar Tiwari <SumitTiwari203@gmail.com>
2018-01-07 21:53:34 -0500

    Added Middleware Redux, thunk

--- src/index.js
+++ src/index.js
@@ -3,13 +3,27 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './components/App';
 import registerServiceWorker from './registerServiceWorker';
-import {createStore} from 'redux';
-import reducers from './reducers';
+import { createStore, applyMiddleware, compose } from 'redux'
+import reducer from './reducers';
 import {Provider} from 'react-redux';
+
+const logger = store => next => action => {
+    console.group(action.type)
+    console.info('dispatching', action)
+    let result = next(action)
+    console.log('next state', store.getState())
+    console.groupEnd(action.type)
+    return result
+}
+
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose
+
 const store = createStore(
-    reducers,
-    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+    reducer,
+    composeEnhancers(
+        applyMiddleware(logger)
     )
+)
 
 ReactDOM.render(<Provider store ={store}>
                     <App/>
